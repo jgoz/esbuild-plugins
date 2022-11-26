7f00c90c3d89742ea8c610052e86c07f8475e7cf
John Gozde <john@gozde.ca>
2022-11-26 11:43:24 -0700

    Stop esbuild in watch mode

--- packages/esbuild-plugin-typecheck/test/fixture/build/pkg-three/build.js
+++ packages/esbuild-plugin-typecheck/test/fixture/build/pkg-three/build.js
@@ -2,20 +2,30 @@
 
 const { typecheckPlugin } = require('../../../lib');
 const { build } = require('esbuild');
-const path = require('path');
 
-build({
-  absWorkingDir: __dirname,
-  entryPoints: ['./three.ts'],
-  bundle: true,
-  format: 'esm',
-  outdir: './dist',
-  platform: 'node',
-  plugins: [
-    typecheckPlugin({
-      buildMode: process.env.BUILD_MODE,
-    }),
-  ],
-  watch: !!process.env.WATCH,
-  write: false,
-}).catch(() => process.exit(1));
+async function main() {
+  try {
+    const result = await build({
+      absWorkingDir: __dirname,
+      entryPoints: ['./three.ts'],
+      bundle: true,
+      format: 'esm',
+      outdir: './dist',
+      platform: 'node',
+      plugins: [
+        typecheckPlugin({
+          buildMode: process.env.BUILD_MODE,
+        }),
+      ],
+      watch: !!process.env.WATCH,
+      write: false,
+    });
+    process.on('SIGTERM', () => {
+      result.stop();
+    });
+  } catch (error) {
+    process.exit(1);
+  }
+}
+
+main();
--- packages/esbuild-plugin-typecheck/test/fixture/compile/build.js
+++ packages/esbuild-plugin-typecheck/test/fixture/compile/build.js
@@ -2,16 +2,26 @@
 
 const { typecheckPlugin } = require('../../lib');
 const { build } = require('esbuild');
-const path = require('path');
 
-build({
-  absWorkingDir: __dirname,
-  entryPoints: ['./src/index.ts'],
-  bundle: true,
-  format: 'esm',
-  outdir: './dist',
-  platform: 'node',
-  plugins: [typecheckPlugin()],
-  watch: !!process.env.WATCH,
-  write: false,
-}).catch(() => process.exit(1));
+async function main() {
+  try {
+    const result = await build({
+      absWorkingDir: __dirname,
+      entryPoints: ['./src/index.ts'],
+      bundle: true,
+      format: 'esm',
+      outdir: './dist',
+      platform: 'node',
+      plugins: [typecheckPlugin()],
+      watch: !!process.env.WATCH,
+      write: false,
+    });
+    process.on('SIGTERM', () => {
+      result.stop();
+    });
+  } catch (error) {
+    process.exit(1);
+  }
+}
+
+main();
