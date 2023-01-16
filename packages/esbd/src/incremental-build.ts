7c8d5800e6e2337998a6bccf7cd69a69753ecc5d
John Gozde <john@gozde.ca>
2023-01-16 11:34:26 -0700

    Remove stale outputs after each build

--- packages/esbd/src/incremental-build.ts
+++ packages/esbd/src/incremental-build.ts
@@ -32,6 +32,26 @@ interface IncrementalBuildContext extends BuildContext<RequiredBuildOptions> {
   wait(): Promise<void>;
 }
 
+async function syncOutputs(
+  result: IncrementalBuildResult,
+  previousOutputs: Set<string>,
+  logger: Logger,
+): Promise<Set<string>> {
+  const outputFiles = new Set(result.outputFiles.map(file => file.path));
+  const staleOutputs = Array.from(previousOutputs.values()).filter(file => !outputFiles.has(file));
+  await Promise.allSettled(
+    staleOutputs.map(async file => {
+      logger.debug(pc.gray(`Removing stale output ${file}`));
+      try {
+        await rm(file);
+      } catch {
+        logger.debug(pc.gray(`Failed to remove stale output ${file}; ignoring`));
+      }
+    }),
+  );
+  return outputFiles;
+}
+
 export async function incrementalBuild({
   cleanOutdir,
   copy,
@@ -46,6 +66,7 @@ export async function incrementalBuild({
   const evt = new EventEmitter();
   const outdir = options.outdir;
   const absOutDir = outdir ? path.resolve(basedir, outdir) : undefined;
+  let previousOutputs = new Set<string>();
 
   const normalizedCopy: [string, string][] = [];
   if (copy) {
@@ -59,6 +80,17 @@ export async function incrementalBuild({
         ]);
       }
     }
+  }
+
+  function copyAssets(fromPath?: string) {
+    return Promise.all(
+      normalizedCopy
+        .filter(([from]) => !fromPath || fromPath === from)
+        .map(([from, to]) => {
+          logger.info(pc.gray(`Copying ${from} to ${to}`));
+          return copyFile(from, to);
+        }),
+    );
   }
 
   const resultPlugin: Plugin = {
@@ -75,6 +107,9 @@ export async function incrementalBuild({
         if (!result.errors.length) {
           logger.debug('Build successful');
           if (absOutDir) await mkdirp(absOutDir);
+          if (cleanOutdir) {
+            previousOutputs = await syncOutputs(result, previousOutputs, logger);
+          }
           await copyAssets();
         } else {
           logger.debug('Build failed');
@@ -94,23 +129,11 @@ export async function incrementalBuild({
     plugins: [resultPlugin, ...(options.plugins ?? [])],
   });
 
-  function copyAssets(fromPath?: string) {
-    return Promise.all(
-      normalizedCopy
-        .filter(([from]) => !fromPath || fromPath === from)
-        .map(([from, to]) => {
-          logger.info(pc.gray(`Copying ${from} to ${to}`));
-          return copyFile(from, to);
-        }),
-    );
-  }
-
-  const abort = new AbortController();
-  const { signal } = abort;
+  const watchAbort = new AbortController();
 
   async function dispose(): Promise<void> {
     await context.dispose();
-    abort.abort();
+    watchAbort.abort();
   }
 
   async function watch(): Promise<void> {
@@ -118,7 +141,7 @@ export async function incrementalBuild({
     await context.watch();
 
     for (const [from] of normalizedCopy) {
-      fsWatch(from, { persistent: false, signal }, event => {
+      fsWatch(from, { persistent: false, signal: watchAbort.signal }, event => {
         if (event === 'change') {
           copyAssets(from).catch(logger.error);
         }
